777991b2c9218550abfac893675efae8e36113fb
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-09-02 15:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const userText = document.getElementById("text").value.trim();
+    const delayText = document.getElementById("delay").value.trim();
+    const output = document.getElementById("output");
+
+    // Convert delay from milliseconds if needed
+    const delayInMs = parseInt(delayText, 10);
+
+    // Async function with delay
+    const delayedDisplay = async (text, delay) => {
+        await new Promise(resolve => setTimeout(resolve, delay));
+        output.innerText = text
+    };
+
+    // Call the async function with the provided values
+    delayedDisplay(userText, delayInMs);
+});
